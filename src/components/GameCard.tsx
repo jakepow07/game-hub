f6f2b532f278ea514b3e7bcc400d07225f50d92c
jakepow07 <65188545+jakepow07@users.noreply.github.com>
2023-11-02 21:21:18 -0400

    Add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -20,7 +20,7 @@ const GameCard = ({ game }: Props) => {
             <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
             <CriticScore score={game.metacritic} />
             </HStack>
-            <Heading fontSize='2xl'>{game.name}</Heading>
+            <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top}/></Heading>
             
             
             </CardBody>
