55ff86b2e2490d95be8f5ff7ebc2858b618518c1
jakepow07 <65188545+jakepow07@users.noreply.github.com>
2023-11-02 21:21:26 -0400

    Add emojis

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,7 +1,7 @@
 import bullsEye from '../assets/bulls-eye.webp'
 import thumbsUp from '../assets/thumbs-up.webp'
 import meh from '../assets/meh.webp'
-import Image, { ImageProps } from '@chakra-ui/react'
+import { Image, ImageProps } from '@chakra-ui/react'
 
 interface Props {
     rating: number;
@@ -11,13 +11,13 @@ const Emoji = ({ rating }: Props) => {
     if (rating < 3) return null;
     
     const emojiMap: { [key: number]: ImageProps } = {
-        3: { src: meh, alt: 'meh'}
-        4: { src: thumbsUp, alt: 'recommended'}
-        5: { src: bullsEye, alt: 'exceptional'}
+        3: { src: meh, alt: 'meh', boxSize: '25px'},
+        4: { src: thumbsUp, alt: 'recommended', boxSize: '25px'},
+        5: { src: bullsEye, alt: 'exceptional', boxSize: '35px'}
     }
   return  (
     
-    <Image {...emojiMap[rating]} boxSize='25px' />
+    <Image {...emojiMap[rating]} marginTop={1}/>
     
     
   )
